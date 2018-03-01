306f27fc8453b92a1482827d721470bee7a3ce14
JacobRoos <jxr5789@grit.com>
2018-02-28 23:18:11 -0500

    Some A* work

---
+++ AI-Display/Assets/Scripts/Heap.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PathHeap
+{
+    class Heap
+    {
+        //key is weighted distance
+        //value is the location
+        List<KeyValuePair<float, KeyValuePair<int, int>>> keys = new List<KeyValuePair<float, KeyValuePair<int, int>>>();
+
+        static int GetParent(int i)
+        {
+            return (i - 1) / 2;
+        }
+
+        static int GetLeft(int i)
+        {
+            return 2 * i + 1;
+        }
+
+        static int GetRight(int i)
+        {
+            return 2 * i + 2;
+        }
+
+        private void HeapifyUp(int i)
+        {
+            if (i <= 0) return;
+
+            //get the index of the parent in the list
+            int j = GetParent(i);
+
+            //check the values at the indexes
+            if (keys[i].Key < keys[j].Key)
+            {
+                KeyValuePair<float, KeyValuePair<int, int>> temp = keys[i];
+                keys[i] = keys[j];
+                keys[j] = temp;
+            }
+
+            HeapifyUp(j);
+        }
+
+        private void HeapifyDn(int i)
+        {
+            int j;
+
+            // If no children...
+            if (GetLeft(i) > keys.Count - 1) return;
+
+            // If no right child...
+            if (GetRight(i) > keys.Count - 1)
+            {
+                j = GetLeft(i);
+            }
+            else
+            {
+                // If both right and left children
+                j = (keys[GetLeft(i)].Key < keys[GetRight(i)].Key) ? (GetLeft(i)) : (GetRight(i));
+            }
+
+            if (keys[i].Key > keys[j].Key)
+            {
+                KeyValuePair<float, KeyValuePair<int, int>> temp = keys[i];
+                keys[i] = keys[j];
+                keys[j] = temp;
+            }
+
+            HeapifyDn(j);
+        }
+
+        public void Insert(float newKey, KeyValuePair<int, int> newValue)
+        {
+            keys.Add(new KeyValuePair<float, KeyValuePair<int, int>>(newKey, newValue));
+            HeapifyUp(keys.Count - 1);
+        }
+
+        public KeyValuePair<float, KeyValuePair<int, int>> Pop()
+        {
+            KeyValuePair<float, KeyValuePair<int, int>> temp = keys[0];
+
+            keys[0] = keys[keys.Count - 1];
+            keys.RemoveAt(keys.Count - 1);
+
+            HeapifyDn(0);
+
+            return temp;
+        }
+
+        public int GetSize()
+        {
+            return keys.Count;
+        }
+    }
+}
---
+++ AI-Display/Assets/Scripts/MapManager.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PathHeap;
+
+public class MapManager : MonoBehaviour {
+
+    private int x = 100;
+    private int y = 100;
+
+    private int size = 1;
+
+    //true: location can be moved to, false: location can't be oved to
+    public bool[,] mapData;
+
+
+	// Use this for initialization
+	void Start () {
+        GetMapData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GetMapData()
+    {
+        //get data from the map
+    }
+
+    public bool CreateRandomPath(int charX, int charY)
+    {
+        //destination values
+        int destX;
+        int destY;
+
+        //generate destination values that area not the characters location
+        do
+        {
+            destX = (int)(Random.Range(0, 1000000) % x);
+            destY = (int)(Random.Range(0, 1000000) % y);
+        }
+        while (destX == charX && destY == charY);
+
+        KeyValuePair<int, int>[,] pathParent = CreatePathPoints(charX, charY, destX, destY);
+
+        if(pathParent == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public KeyValuePair<int, int>[,] CreatePathPoints(int startX, int startY, int destX, int destY)
+    {
+        //2D array that holds the data of the parent node to calculate the path later
+        KeyValuePair<int, int>[,] pathParent = new KeyValuePair<int, int>[x, y];
+
+        //2D array that keeps track of visited nodes
+        bool[,] availableNodes = new bool[x,y];
+
+
+        //copy the data from the  map, get blocked nodes
+        for(int i = 0; i < x; i++)
+        {
+            for(int j = 0; j < y; j++)
+            {
+                availableNodes[i, j] = mapData[i, j];
+            }
+        }
+
+        //create a priority queue for node data
+        Heap prq = new Heap();
+
+        //double to keep track of the heuristic value
+        float heur = 0;
+
+        //ints to hold data in the checking of locations
+        int nextNodeX;
+        int nextNodeY;
+
+        //insert the start location
+        prq.Insert(0, new KeyValuePair<int, int>(startX, startY));
+
+        //place a signal value in the pathParents
+        pathParent[startX, startY] = new KeyValuePair<int, int>(-1, -1);
+
+        availableNodes[startX, startY] = false;
+
+        while (true)
+        {
+            //get the next location from the priority queue
+            KeyValuePair<float, KeyValuePair<int, int>> currentNodeData = prq.Pop();
+
+            if(currentNodeData.Value.Key == destX && currentNodeData.Value.Value == destY)
+            {
+                //break out of the loop
+                break;
+            }
+
+
+            //get value of the node to the left
+            nextNodeX = currentNodeData.Value.Key - 1;
+            nextNodeY = currentNodeData.Value.Value;
+
+            //check to the left
+            if (nextNodeX >= 0)
+            {
+                //check if the tile is available
+                if(availableNodes[nextNodeX, nextNodeY])
+                {
+                    //calculate the heuristic to the destination
+                    heur = Mathf.Sqrt( (destX - nextNodeX) * (destX - nextNodeX) + (destY - nextNodeY) * (destY - nextNodeY));
+
+                    //add the new node to the heap with the weighted heuristic value
+                    prq.Insert(currentNodeData.Key + 1 + heur, new KeyValuePair<int, int>(nextNodeX, nextNodeY));
+
+                    //add the parent location of this new node
+                    pathParent[nextNodeX, nextNodeY] = currentNodeData.Value;
+
+                    //update the available node array
+                    availableNodes[nextNodeX, nextNodeY] = false;
+                }
+            }
+
+
+            //get value of the node to the right
+            nextNodeX = currentNodeData.Value.Key + 1;
+            nextNodeY = currentNodeData.Value.Value;
+
+            //check to the right
+            if (nextNodeX < x)
+            {
+                //check if the tile is available
+                if (availableNodes[nextNodeX, nextNodeY])
+                {
+                    //calculate the heuristic to the destination
+                    heur = Mathf.Sqrt((destX - nextNodeX) * (destX - nextNodeX) + (destY - nextNodeY) * (destY - nextNodeY));
+
+                    //add the new node to the heap with the weighted heuristic value
+                    prq.Insert(currentNodeData.Key + 1 + heur, new KeyValuePair<int, int>(nextNodeX, nextNodeY));
+
+                    //add the parent location of this new node
+                    pathParent[nextNodeX, nextNodeY] = currentNodeData.Value;
+
+                    //update the available node array
+                    availableNodes[nextNodeX, nextNodeY] = false;
+                }
+            }
+
+
+            //get value of the node from above
+            nextNodeX = currentNodeData.Value.Key;
+            nextNodeY = currentNodeData.Value.Value - 1;
+
+            //check above
+            if (nextNodeY >= 0)
+            {
+                //check if the tile is available
+                if (availableNodes[nextNodeX, nextNodeY])
+                {
+                    //calculate the heuristic to the destination
+                    heur = Mathf.Sqrt((destX - nextNodeX) * (destX - nextNodeX) + (destY - nextNodeY) * (destY - nextNodeY));
+
+                    //add the new node to the heap with the weighted heuristic value
+                    prq.Insert(currentNodeData.Key + 1 + heur, new KeyValuePair<int, int>(nextNodeX, nextNodeY));
+
+                    //add the parent location of this new node
+                    pathParent[nextNodeX, nextNodeY] = currentNodeData.Value;
+
+                    //update the available node array
+                    availableNodes[nextNodeX, nextNodeY] = false;
+                }
+            }
+
+
+            //get value of the node from below
+            nextNodeX = currentNodeData.Value.Key;
+            nextNodeY = currentNodeData.Value.Value + 1;
+
+            //check below
+            if (nextNodeY < y)
+            {
+                //check if the tile is available
+                if (availableNodes[nextNodeX, nextNodeY])
+                {
+                    //calculate the heuristic to the destination
+                    heur = Mathf.Sqrt((destX - nextNodeX) * (destX - nextNodeX) + (destY - nextNodeY) * (destY - nextNodeY));
+
+                    //add the new node to the heap with the weighted heuristic value
+                    prq.Insert(currentNodeData.Key + 1 + heur, new KeyValuePair<int, int>(nextNodeX, nextNodeY));
+
+                    //add the parent location of this new node
+                    pathParent[nextNodeX, nextNodeY] = currentNodeData.Value;
+
+                    //update the available node array
+                    availableNodes[nextNodeX, nextNodeY] = false;
+                }
+            }
+
+
+            if(prq.GetSize() == 0)
+            {
+                return null;
+            }
+        }
+
+        return pathParent;
+
+
+    }
+}
