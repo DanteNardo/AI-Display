3c1b35e75817a29b5c634b2c07d13e873e63486c
zanthous <bcm023@gmail.com>
2018-03-03 17:11:42 -0500

    flocking stuff
    select manager in editor to show the path they are running through, it's randomized. breaks if one gets separated crossing the bridge

---
+++ AI-Display/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,253 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class SceneManager : MonoBehaviour
+{
+    public List<GameObject> flockers;
+    private GameObject[] temp;
+
+    public GameObject target;
+    private Vector3 avgDirection;
+    private Vector3 avgPosition;
+
+    private bool debugMode;
+
+    public Material blue;
+    public Material green;
+
+    private List<GameObject> obstacles;
+    
+    private Terrain terrain;
+    private int cornerIndex;
+    private GameObject endGoal;
+    private NavMeshPath path;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        flockers = new List<GameObject>();
+        temp = new GameObject[GameObject.FindGameObjectsWithTag("Flocker").Length];
+        temp = GameObject.FindGameObjectsWithTag("Flocker");
+        foreach (GameObject g in temp)
+        {
+            flockers.Add(g);
+        }
+
+        this.target = GameObject.FindGameObjectWithTag("Target");
+
+        avgDirection = Vector3.zero;
+        avgPosition = Vector3.zero;
+
+        terrain = GameObject.Find("Terrain").GetComponent<Terrain>();
+        endGoal = GameObject.Find("EndGoal");
+
+        path = new NavMeshPath();
+
+
+        NavMesh.CalculatePath(flockers[0].transform.position,endGoal.transform.position,1<<NavMesh.GetAreaFromName("Walkable"), path);
+
+        cornerIndex = 0;
+        target.transform.position = path.corners[cornerIndex];
+
+
+      
+
+
+        temp = new GameObject[ GameObject.FindGameObjectsWithTag( "Obstacle" ).Length ];
+        temp = GameObject.FindGameObjectsWithTag( "Obstacle" );
+        obstacles = new List<GameObject>();
+        foreach( GameObject g in temp )
+        {
+            obstacles.Add( g );
+        }
+        foreach (GameObject g in flockers)
+        {
+            g.GetComponent<Flocker>().target = target;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if( Input.GetKeyDown(KeyCode.D))
+        {
+            debugMode = !debugMode;
+        }
+
+        avgDirection = Vector3.zero;
+        avgPosition = Vector3.zero;
+        //in case number of flockers changes
+
+        //convert array to list
+
+
+        //average the position and direction
+        foreach( GameObject g in flockers )
+        {
+            //if( g.GetComponent<Flocker>().direction != Vector3.zero )
+                avgDirection += g.GetComponent<Flocker>().transform.forward;
+            //if( g.GetComponent<Flocker>().position != Vector3.zero )
+                avgPosition += g.GetComponent<Flocker>().position;
+        }
+
+        avgDirection = avgDirection.normalized;
+
+
+
+        avgDirection *= flockers[ 0 ].GetComponent<VehicleMovement>().maxSpeed;
+        avgPosition /= flockers.Count;
+        GameObject.Find( "AveragePosition" ).transform.position = avgPosition;
+        if( avgDirection != Vector3.zero )
+        {
+            GameObject.Find( "AveragePosition" ).transform.rotation = Quaternion.LookRotation( avgDirection );
+        }
+        //send information to flockers
+        foreach( GameObject g in flockers )
+        {
+            g.GetComponent<Flocker>().avgDirection = this.avgDirection;
+            g.GetComponent<Flocker>().avgPosition = this.avgPosition;
+        }
+
+        //find closest flocker to every flocker
+        //outer is the one we are comparing with
+        for( int outer = 0; outer < flockers.Count; outer++ )
+        {
+            float distance = int.MaxValue;
+            int indexOfClosest = int.MaxValue;
+            //inner to
+            for( int inner = 0; inner < flockers.Count; inner++ )
+            {
+                if( flockers[outer] != flockers[inner] && Vector3.Distance( flockers[ outer ].transform.position, flockers[ inner ].transform.position ) < distance )
+                {
+                    distance = Vector3.Distance( flockers[ outer ].transform.position, flockers[ inner ].transform.position );
+                    indexOfClosest = inner;
+                }
+            }
+            if( indexOfClosest != int.MaxValue )
+            { 
+                flockers[ outer ].GetComponent<Flocker>().nearestNeighbor = flockers[ indexOfClosest ];
+            }
+        }
+
+
+        for( int outer = 0; outer < flockers.Count; outer++ )
+        {
+            float distance = int.MaxValue;
+            int indexOfClosest = int.MaxValue;
+
+            for( int inner = 0; inner < obstacles.Count; inner++ )
+            {
+                if( Vector3.Distance( ProjectToXZ( flockers[outer].transform.position), ProjectToXZ( obstacles[inner].transform.position) ) < distance )
+                {
+                    distance = Vector3.Distance( ProjectToXZ( flockers[ outer ].transform.position), ProjectToXZ( obstacles[ inner ].transform.position ) );
+                    indexOfClosest = inner;
+                }
+            }
+            if( indexOfClosest != int.MaxValue )
+            {
+                flockers[ outer ].GetComponent<Flocker>().nearestObstacle = obstacles[ indexOfClosest ];
+            }
+        }
+
+        //update target
+        for (int index = 0; index < flockers.Count; index++)
+        {
+            //if flocker and target are within .7 units, reset position of goal
+            if (Vector3.Distance(ProjectToXZ(flockers[index].transform.position), ProjectToXZ(target.transform.position)) < 1f)
+            {
+                cornerIndex++;
+                if(cornerIndex >= path.corners.Length)
+                {
+                    endGoal.transform.position = new Vector3(Random.value * 80, 0.0f, Random.value * 80);
+                    endGoal.transform.position += new Vector3(0, GetHeight(endGoal.transform.position), 0);
+
+                    while (!(NavMesh.CalculatePath(flockers[0].transform.position, endGoal.transform.position, 1 << NavMesh.GetAreaFromName("Walkable"), path)))
+                    {
+                        endGoal.transform.position = new Vector3(Random.value * 80, 0.0f, Random.value * 80);
+                        endGoal.transform.position += new Vector3(0, GetHeight(endGoal.transform.position), 0);
+                    }
+
+                    cornerIndex = 0;
+
+                    target.transform.position = path.corners[cornerIndex];
+                    break;
+                }
+
+                target.transform.position = path.corners[cornerIndex];
+            }
+        }
+
+
+    }
+    Vector3 ProjectToXZ( Vector3 input )
+    {
+        return new Vector3(input.x, 0, input.z);
+    }
+
+    //void OnRenderObject()
+    //{
+    //    if( debugMode )
+    //    {
+    //        //forward
+    //        blue.SetPass( 0 );
+    //        GL.Begin( GL.LINES );
+    //        GL.Vertex( GameObject.Find( "AveragePosition" ).transform.position );
+    //        GL.Vertex( GameObject.Find( "AveragePosition" ).transform.position + GameObject.Find( "AveragePosition" ).transform.forward * 5 );
+    //        GL.End();
+
+    //        GameObject.Find( "AveragePosition" ).GetComponentInChildren<MeshRenderer>().enabled = true;
+    //    }
+    //    else
+    //    {
+    //        GameObject.Find( "AveragePosition" ).GetComponentInChildren<MeshRenderer>().enabled = false;
+    //    }
+    //}
+
+    void OnDrawGizmosSelected()
+    {
+        if(path != null)
+        { 
+            Gizmos.color = Color.red;
+        
+            foreach( Vector3 v in path.corners)
+            {
+                Gizmos.DrawSphere(v, 1);
+            }
+        }
+    }
+
+    public float GetHeight(Vector3 input)
+    {
+        RaycastHit hit;
+
+        Ray downRay;
+
+        //ray pointing down from location
+        downRay = new Ray(input, Vector3.down);
+
+        if (Physics.Raycast(downRay, out hit))
+        {
+            return hit.point.y + .5f;
+        }
+
+        //incase we are below the terrain
+        else
+        {
+
+            downRay = new Ray(input, Vector3.up);
+
+            if (Physics.Raycast(downRay, out hit))
+            {
+                return hit.point.y + .5f;
+            }
+        }
+
+        return 0.0f;
+    }
+
+}
---
+++ AI-Display/Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,304 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public abstract class VehicleMovement : MonoBehaviour
+{
+
+    // Reference to this vehicle's target
+    public GameObject target;
+
+    // Vectors for force-based movement
+    public Vector3 position;
+    public Vector3 direction;
+    public Vector3 acceleration;
+    public Vector3 velocity;
+
+    protected Vector3 ultimateForce;
+    // UNNECESSARY FOR NOW
+    // forces to be applied to this vehicle
+    //public Vector3 wind;
+    //public Vector3 gravity;
+
+    // floats necessary for STEERING FORCES
+    public float mass;
+    public float maxSpeed;
+    public float radius;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CalcSteeringForces();
+        UpdatePosition();
+        SetPosition();
+        SetTransform();
+
+    }
+    public Vector3 Avoid2D( GameObject obj )
+    {
+        //obstacles have raidus of 1
+        //zombies/humans have a radius of .15
+        //1.15 is the sum of the radii
+        Vector3 toCenter;
+        Vector3 right = this.transform.right;
+        Vector3 result = new Vector3( 0, 0, 0 );
+
+        toCenter =  obj.transform.position - this.transform.position;
+        toCenter.y = 0.0f;
+
+        if( toCenter.magnitude - 3 > /*safedistance*/ 5 )
+        {
+            //no potential collsion
+            return new Vector3( 0, 0, 0 ); ;
+        }
+        else
+        {
+            if( Vector3.Dot( toCenter, this.transform.forward ) < 0 )
+            {
+                return new Vector3( 0, 0, 0 ); ;
+            }
+            else
+            {
+                if( Vector3.Dot( toCenter, right ) > 3 )
+                {
+                    return new Vector3( 0, 0, 0 ); ;
+                }
+                else
+                {
+
+                    if( Vector3.Dot( toCenter, right ) > 0 ) // is to the right
+                    {
+                        result = -right * maxSpeed;
+                        result -= this.velocity;
+                    }
+                    else if( Vector3.Dot( toCenter, right ) < 0 ) // is to the left
+                    {
+                        result = right * maxSpeed;
+                        result -= this.velocity;
+                    }
+                }
+            }
+
+        }
+        //calc steering force
+
+
+        return result;
+
+
+    }
+    public Vector3 Avoid( GameObject obj )
+    {
+        //obstacles have raidus of 1
+        //zombies/humans have a radius of .15
+        //1.15 is the sum of the radii
+        Vector3 toCenter;
+        Vector3 right = this.transform.right;
+        Vector3 result = new Vector3( 0, 0, 0 );
+
+        toCenter =  obj.transform.position - this.transform.position;
+
+        if( toCenter.magnitude - 1.15 > /*safedistance*/ 1.2 )
+        {
+            //no potential collsion
+            return new Vector3( 0, 0, 0 ); ;
+        }
+        else
+        {
+            if( Vector3.Dot( toCenter, this.transform.forward ) < 0 )
+            {
+                return new Vector3( 0, 0, 0 ); ;
+            }
+            else
+            {
+                if( Vector3.Dot( toCenter, right ) > 1.15 )
+                {
+                    return new Vector3( 0, 0, 0 ); ;
+                }
+                else
+                {
+
+                    if( Vector3.Dot( toCenter, right ) > 0 ) // is to the right
+                    {
+                        result = -right * maxSpeed;
+                        result -= this.velocity;
+                    }
+                    else if( Vector3.Dot( toCenter, right ) < 0 ) // is to the left
+                    {
+                        result = right * maxSpeed;
+                        result -= this.velocity;
+                    }
+                }
+            }
+
+        }
+        //calc steering force
+
+
+        return result;
+
+
+    }
+    
+
+    /// <summary>
+    /// UpdatePosition
+    /// Calculate a new position for this vehicle based on incoming forces
+    /// </summary>
+    void UpdatePosition()
+    {
+        // Grab the world position from the transform component
+        position = gameObject.transform.position;
+
+        // Step 1: Add accel to vel * time
+        velocity += acceleration * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude( velocity, maxSpeed );
+        // Step 2: Add velocity to position
+        position += velocity * Time.deltaTime;
+
+        // Step 3: Derive a direction (
+        if( velocity != Vector3.zero )
+        {
+            direction = velocity.normalized;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler( Vector3.zero );
+        }
+        // Step 4: Zero out acceleration
+        // (start fresh with new forces every frame)
+        acceleration = Vector3.zero;
+    }
+
+    /// <summary>
+    /// Applies the force to the vehicle. 
+    /// </summary>
+    /// <param name="force">Force.</param>
+    protected virtual void ApplyForce( Vector3 force )
+    {
+        acceleration += force / mass;
+    }
+
+
+    /// <summary>
+    /// Apply friction to a vehicle 
+    /// </summary>
+    void ApplyFriction( float coeff )
+    {
+        // Step 1: Get the negative velocity
+        Vector3 friction = velocity * -1;
+
+        // Step 2: Normalize it (friction is not dependent on vel mag)
+        friction.Normalize();
+
+        // Step 3: Multiply by coefficient of friction
+        friction = friction * coeff;
+
+        // Step 4: Add to acceleration
+        acceleration += friction;
+    }
+
+
+    public Vector3 Seek( Vector3 targetPosition )
+    {
+        // Step 1:  Calculate desired velocity
+        //  This is the vector pointing from myself to my target
+        Vector3 desiredVelocity = targetPosition - position;
+
+        // Step 2: Scale desired to the maximum speed
+        //   so that I move as quickly as possible
+        //desiredVelocity = Vector3.ClampMagnitude (desiredVelocity, maxSpeed);
+
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Step 3: Calculate the steering force for seeking
+        // Steering = desred - current
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Step 4: Return the steering force so this vehicle can 
+        //   aply it to its acceleration vector
+        return steeringForce;
+    }
+
+    public Vector3 Seek2D( Vector3 targetPosition )
+    {
+        // Step 1:  Calculate desired velocity
+        //  This is the vector pointing from myself to my target
+        Vector3 xzProjTarget = new Vector3(targetPosition.x,0.0f,targetPosition.z);
+
+        Vector3 desiredVelocity = xzProjTarget - new Vector3( position.x,0.0f, position.z);
+
+        // Step 2: Scale desired to the maximum speed
+        //   so that I move as quickly as possible
+        //desiredVelocity = Vector3.ClampMagnitude (desiredVelocity, maxSpeed);
+
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Step 3: Calculate the steering force for seeking
+        // Steering = desred - current
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Step 4: Return the steering force so this vehicle can 
+        //   aply it to its acceleration vector
+        return steeringForce;
+    }
+
+    public Vector3 Flee2D( Vector3 target )
+    {
+        Vector3 xzProjTarget = new Vector3(target.x,0,target.z);
+        Vector3 desiredVelocity = xzProjTarget - new Vector3(position.x,0,position.z);
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        return -steeringForce;
+    }
+
+    public Vector3 Flee( Vector3 target )
+    {
+        Vector3 desiredVelocity = target - position;
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        return -steeringForce;
+    }
+
+    /// <summary>
+    /// Applies any fore as a gravity force, where the result is independent of the mass
+    /// </summary>
+    /// <param name="force">Force.</param>
+    void ApplyGravity( Vector3 force )
+    {
+        acceleration += force;
+    }
+
+    /// <summary>
+    /// Set the transform component to reflect the local position vector
+    /// </summary>
+    void SetTransform()
+    {
+        // Rotate this vehicle based on its up vector
+        // Unity will rotate the game object to face the correct way
+        if( direction != Vector3.zero )
+        { 
+            gameObject.transform.forward = direction;
+        }
+        gameObject.transform.position = position;
+
+        
+    }
+
+
+    public abstract void CalcSteeringForces();
+
+    public abstract void SetPosition();
+
+    protected virtual void Start()
+    {
+        
+    }
+}
