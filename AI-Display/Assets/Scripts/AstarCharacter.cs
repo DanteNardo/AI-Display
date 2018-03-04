d26b8819eb097c86449959bacab664baa5a979a4
JacobRoos <jxr5789@grit.com>
2018-03-04 17:23:34 -0500

    added comments and fixed an error I found

--- AI-Display/Assets/Scripts/AstarCharacter.cs
+++ AI-Display/Assets/Scripts/AstarCharacter.cs
@@ -8,19 +8,21 @@ using UnityEngine;
 
 public class AstarCharacter : MonoBehaviour
 {
-
+    //reference to the mapManager to call functions
     public GameObject mapManager;
 
+    //location in the grid
     public int xLoc;
-
     public int yLoc;
 
+    //the path to follow
     public List<Vector2> path;
 
+    //raycasting variables
     private RaycastHit hit;
-
     private Ray downRay;
 
+    //bool to determine if the character is active
     private bool active;
 
     // Use this for initialization
@@ -32,47 +34,55 @@ public class AstarCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if the character is active, Follow the path it has
         if(active)
         {
             FollowPath();
         }
     }
 
+    //assign a new path, used in MapManager
     public void AssignPath(List<Vector2> newPath)
     {
         path = newPath;
     }
 
+    //have the character follow the path it has
     private void FollowPath()
     {
+        //if there is a path
         if(path != null && path.Count > 0)
         {
             // move in path direction
-
-
             this.transform.position += (new Vector3(path[0].x - this.transform.position.x, GetHeight(), path[0].y - this.transform.position.z)).normalized / 10;
 
+            //if the character has reached the next node in the path
             if (Vector2.Distance(path[0], new Vector2(this.transform.position.x, this.transform.position.z)) < .05)
             {
+                //update the grid location
                 xLoc = (int)(path[0].x + mapManager.GetComponent<MapManager>().x / 2 - .5f);
+                yLoc = (int)(-1 * path[0].y + mapManager.GetComponent<MapManager>().y / 2 - .5f);
 
-                yLoc = (int)(-1 * path[0].y + mapManager.GetComponent<MapManager>().y / 2 + .5f);
-
+                //remove this path node to now move toward the next node
                 path.RemoveAt(0);
             }
         }
 
+        //if there is no path or we have reached the end
         else
         {
-            path = mapManager.GetComponent<MapManager>().CreateRandomPath(xLoc, yLoc - 1);
+            //create a new path to follow
+            path = mapManager.GetComponent<MapManager>().CreateRandomPath(xLoc, yLoc);
         }
     }
 
+    //get the height of the character so it will hover above the ground at a set height
     public float GetHeight()
     {
         //ray pointing down from location
         downRay = new Ray(this.transform.position, -1 * Vector3.up);
 
+        //if the character is above the terrain
         if(Physics.Raycast(downRay, out hit))
         {
             return 1 - hit.distance;
@@ -85,8 +95,5 @@ public class AstarCharacter : MonoBehaviour
 
             return 1 + hit.distance;
         }
-
-
-
     }
 }
