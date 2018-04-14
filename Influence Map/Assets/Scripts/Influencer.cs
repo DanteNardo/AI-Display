698fa0a99fcd9ac9d95ef10bdf530aeb373306b5
JacobRoos <jxr5789@grit.com>
2018-04-14 00:24:12 -0400

    Added influencer placement

---
+++ Influence Map/Assets/Scripts/Influencer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Influencer : MonoBehaviour {
+
+    public int influenceVal = 0;
+
+    public int xLoc;
+    public int yLoc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMapLocation(int xIn, int yIn)
+    {
+        xLoc = xIn;
+        yLoc = yIn;
+    }
+
+    public KeyValuePair<int, int> GetMapLocation()
+    {
+        return new KeyValuePair<int, int>(xLoc, yLoc);
+    }
+}
